15055e2ce9092e13a37e92cd42189b9689224a63
Nick Fahey <nfahey@live.com.au>
2025-09-18 11:07:41 +1000

    Build file structure

---
+++ app/routes/ai/routes.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+
+from .messages.routes import router as router_messages
+from .usage.routes import router as router_usage
+
+router = APIRouter(prefix='/AI')
+router.include_router(router_messages)
+router.include_router(router_usage)
---
+++ app/routes/main.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+
+from .ai.routes import router as ai_router
+from .bank.routes import router as bank_router
+from .scores.routes import router as scores_router
+from .users.routes import router as users_router
+
+router = APIRouter(prefix='/api/v1')
+router.include_router(ai_router)
+router.include_router(bank_router)
+router.include_router(scores_router)
+router.include_router(users_router)
