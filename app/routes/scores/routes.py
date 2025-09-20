63f522b67d8c8919c1874fe7016ad257ec191011
Nick Fahey <nfahey@live.com.au>
2025-09-20 15:25:57 +1000

    Removed testing endpoint

--- app/routes/scores/routes.py
+++ app/routes/scores/routes.py
@@ -6,10 +6,6 @@ from .functions import AddScoreByGameName
 
 router = APIRouter(prefix='/scores')
 
-@router.get('')
-def scores_test() -> JSONResponse:
-    return JSONResponse({'message': 'Scores, get'}), 200
-
 @router.post('/add_score', response_model=SingleScoreSchema, responses={404: {'model':HTTPError}})
 async def add_score(data: AddScoreByNameRequestSchema, function: AddScoreByGameName = Depends(AddScoreByGameName)) -> SingleScoreSchema:
     new_score = await function.execute(data.uid, data.game_name, data.amount_won)
